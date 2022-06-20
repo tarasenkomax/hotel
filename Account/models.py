b526e84c5acef4d0fdb0569c2c8d794cff066a50
Максим Тарасенко <mtarasenko@dataplus.ru>
2022-06-20 19:46:00 +0300

    Оптимизировал запросы к базе

--- Account/models.py
+++ Account/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 def user_directory_path(instance, filename):
--- Room/views.py
+++ Room/views.py
@@ -15,7 +15,7 @@ import datetime as DT
 class AllRooms(ListView):
     """ Список всех существующих комнат """
     context_object_name = 'rooms'
-    queryset = Room.objects.all().order_by('number')
+    queryset = Room.objects.all().order_by('number').select_related('type', 'photos_of_room')
     template_name = "rooms/all_rooms.html"
     paginate_by = 6
 
@@ -27,12 +27,13 @@ class DetailRoom(DetailView):
     template_name = "rooms/detail_room.html"
 
     def get_object(self, queryset=None):
-        return get_object_or_404(Room, number=self.kwargs.get("number"))
+        return Room.objects.select_related('type', 'photos_of_room').get(number=self.kwargs.get("number"))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['regulations_list'] = Regulations.objects.filter(type_room=self.get_object().type)
-        context['review_list'] = Review.objects.filter(room=self.get_object()).order_by('-pub_date')
+        context['review_list'] = Review.objects.filter(room=self.get_object()).order_by('-pub_date').select_related(
+            'author')
         context['num_of_review'] = len(context['review_list'])
         return context
 
@@ -43,7 +44,7 @@ class AllReserves(LoginRequiredMixin, ListView):
     template_name = "rooms/all_reserves.html"
 
     def get_queryset(self):
-        return Reserve.objects.filter(client=self.request.user).order_by('-id')
+        return Reserve.objects.filter(client=self.request.user).order_by('-id').select_related('review')
 
 
 class AddReview(LoginRequiredMixin, CreateView):
@@ -58,7 +59,7 @@ class AddReview(LoginRequiredMixin, CreateView):
         return super().post(request, *args, **kwargs)
 
     def get_object(self, queryset=None):
-        reserve = get_object_or_404(Reserve, pk=self.kwargs.get("pk"))
+        reserve = Reserve.objects.select_related('client', 'room').get(pk=self.kwargs.get("pk"))
         if not reserve.client == self.request.user:
             raise Http404
         return reserve
@@ -78,18 +79,20 @@ class AddReview(LoginRequiredMixin, CreateView):
 
 
 class ReserveRoom(LoginRequiredMixin, DetailView):
-    """ Бронирование коматы  """
+    """ Бронирование комнаты  """
     model = Room
     context_object_name = "room"
     template_name = "rooms/reserve_room.html"
 
     def get_object(self, queryset=None):
-        return get_object_or_404(Room, number=self.kwargs.get("number"))
+        return Room.objects.select_related('type', 'photos_of_room').get(number=self.kwargs.get("number"))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['regulations_list'] = Regulations.objects.filter(type_room=self.get_object().type)
-        context['review_list'] = Review.objects.filter(room=self.get_object()).order_by('-pub_date')
+        context['review_list'] = Review.objects.filter(room=self.get_object()).order_by('-pub_date').select_related(
+            'author')
+
         context['num_of_review'] = len(context['review_list'])
         context['day_in'] = self.request.GET['day_in']
         context['day_out'] = self.request.GET['day_out']
@@ -107,7 +110,7 @@ class Cancel(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('all_reserves')
 
     def get_object(self, queryset=None):
-        reserve = get_object_or_404(Reserve, pk=self.kwargs.get("pk"))
+        reserve = Reserve.objects.select_related('client').get(pk=self.kwargs.get("pk"))
         if not reserve.client == self.request.user:
             raise Http404
         return reserve
@@ -133,7 +136,8 @@ class Cancel(LoginRequiredMixin, DeleteView):
                 context['days'] = get_number_of_days(DT.datetime.now().date(), self.get_object().day_out)
                 context['delay'] = False
         cost = self.get_object().room.price * context['days']
-        context['message'] = f"Вам вернется стоимость за {context['days']} дней с {self.get_object().day_in} по {self.get_object().day_out} в размере {cost} рублей. "
+        context[
+            'message'] = f"Вам вернется стоимость за {context['days']} дней с {self.get_object().day_in} по {self.get_object().day_out} в размере {cost} рублей. "
         return context
 
 
@@ -144,18 +148,18 @@ class ListFreeRooms(ListView, LoginRequiredMixin):
     paginate_by = 6
 
     def get_queryset(self):
-        free_rooms_list = Room.objects.filter(number_of_guests__gte=int(self.request.GET['number_of_guests'])).order_by(
-            'number')
-        qs = []
+        free_rooms_list = Room.objects.select_related('type').filter(
+            number_of_guests__gte=int(self.request.GET['number_of_guests']))
+        free_rooms_number_list = []
         for room in free_rooms_list:
             if check_availability(room,
                                   convert_str_to_date(self.request.GET['day_in']),
                                   convert_str_to_date(self.request.GET['day_out'])):
-                qs.append(room)
+                free_rooms_number_list.append(room.number)
         if check_dates_of_user(self.request.user,
                                convert_str_to_date(self.request.GET['day_in']),
                                convert_str_to_date(self.request.GET['day_out'])):
-            return qs
+            return Room.objects.select_related('type').filter(number__in=free_rooms_number_list).order_by('number')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -167,6 +171,7 @@ class ListFreeRooms(ListView, LoginRequiredMixin):
 
 class Pay(LoginRequiredMixin, View):
     """ Оплата брони (заглушка)"""
+
     def get(self, request, *args, **kwargs):
         if check_dates_of_user(request.user,
                                convert_str_to_date(request.GET['day_in']),
